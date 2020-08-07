18f704957c5e0b1a71f77a2fd676f4d11ab01b33
Sakharov Denys <sakharov92@gmail.com>
2020-08-07 15:27:56 +0300

    add build folder

---
+++ SPA-FLOWER-SHOP/js/COMPONENTS/Data.js
@@ -0,0 +1,26 @@
+export const DATA = [
+    {
+        id: "element1",
+        name: "Item Name 1",
+        img: "../../img/flowers.png",
+        price: "28.50",
+    },
+    {
+        id: "element2",
+        name: "Item Name 2",
+        img: "../../img/flowers.png",
+        price: "28.50",
+    },
+    {
+        id: "element3",
+        name: "Item Name 3",
+        img: "../../img/flowers.png",
+        price: "28.50",
+    },
+    {
+        id: "element4",
+        name: "Item Name 4",
+        img: "../../img/flowers.png",
+        price: "28.50",
+    }
+]
